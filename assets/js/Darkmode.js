ae0d0b1f090c82f4d8aa900f38cfccc1b1467d82
Hebertsfj <hebertsdefreitas@gmail.com>
2023-11-21 18:21:33 -0300

    alterações de paginas

---
+++ assets/js/Darkmode.js
@@ -0,0 +1,2 @@
+const chk = document.getElementById('chk')
+chk.addEventListener('change', ()=>{document.body.classList.toggle('dark')})
---
+++ cadastro.js
@@ -0,0 +1,22 @@
+function validarNome () {
+      
+}
+
+function validarLogin () {
+
+}
+
+function validarSenha () {
+
+}
+
+function validarConfirmSenha () {
+
+}
+
+function validarForms () {
+    if (validarNome() && validarLogin() && validarSenha() && validarConfirmSenha() == true){
+        
+    }
+}
+
